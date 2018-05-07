69f0a85b7e186432051cfa91250024f31bf1bb1c
muxue <418605872@qq.com>
2018-05-07 11:42:31 +0800

    Add files via upload

--- nodejs-test/server.js
+++ nodejs-test/server.js
@@ -3,63 +3,48 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if (!port) {
+if(!port){
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url
+  var pathWithQuery = request.url 
   var queryString = ''
-  if (pathWithQuery.indexOf('?') >= 0) {
-    queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
-  }
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
-
-  if (path === '/') {
-    var string = fs.readFileSync('./index.html', 'utf-8')
-    var amount = fs.readFileSync('./db', 'utf-8')
-    string = string.replace('&&&amount&&&', amount)
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(string)
-    response.end()
-  } else if (path === '/main.js') {
-    var string = fs.readFileSync('./main.js', 'utf-8')
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'application/javascript')
-    response.write(string)
-    response.end()
-  } else if (path === '/style.css') {
-    var string = fs.readFileSync('./style.css', 'utf-8')
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css')
-    response.write(string)
-    response.end()
-  } else if (path === '/pay'){
-    let amount = fs.readFileSync('./db', 'utf8')
-    amount -= 1
-    fs.writeFileSync('./db', amount)
-    let callbackName = query.callback
-    response.setHeader('Content-Type', 'application/javascript')
-    response.write(`
-        ${callbackName}.call(undefined, 'success')
-    `)
-
-    response.end()
-} else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('呜呜呜')
-    response.end()
+  
+  if(path === '/'){
+	var string = fs.readFileSync('./index.html', 'utf8')
+	var amount = fs.readFileSync('./db','utf8')
+	string = string.replace('&&&amount&&&',amount)
+	response.statusCode = 200
+	response.setHeader('Content-Type','text/html;charset=utf-8')
+	response.write(string)
+	response.end()
+  }else if(path === '/pay'){
+	var amount = fs.readFileSync('./db','utf8')
+	var newAmount = amount - 1
+	fs.writeFileSync('./db',newAmount)
+	response.setHeader('Content-Type', 'application/javascript')
+	response.statusCode = 200
+	response.write(`
+		${query.callback}.call(undefined,'success')
+	`)
+	response.end() 
+  }else{
+   	response.statusCode = 404
+	response.setHeader('Content-Type','text/html;charset=utf-8')
+	response.write('找不到对应路径')
+	response.end()
   }
+
 
   /******** 代码结束，下面不要看 ************/
 })
